a32859179224a0a79096b41cc711c73a01662b7d
Alexis <alkaloge@cern.ch>
2020-10-07 06:23:03 -0500

    fixes

--- sendJobs.py
+++ sendJobs.py
@@ -1,5 +1,8 @@
+from __future__ import division
 import os
-
+import subprocess
+from decimal import *
+getcontext().prec = 2
 def getArgs() :
     import argparse
     parser = argparse.ArgumentParser()
@@ -46,6 +49,11 @@ command = "dasgoclient --query={0:s} --limit=0  > fileList.txt".format(query)
 print("Running in {0:s} mode.  Command={1:s}".format(args.mode,command))
 os.system(command)
 
+
+
+
+
+
 files = open('fileList.txt','r').readlines()
 if len(files) < 1 :
     print("***In makeCondor.py: Empty fileList.txt")
@@ -65,10 +73,29 @@ for nFiles, file in enumerate(files) :
     if str(args.islocal.lower())=='yes' : fileName = fileName.replace('/eos/uscms','')
     if '#' not in fileName :  dataset.append(fileName)
 
-events=1000
+    #query = '"file={0:s} | grep file.nevents"'.format(fileName)
+    #command = "dasgoclient --query={0:s}  > nevents.txt".format(query)
 
+    query = '"file dataset={0:s} | grep file.nevents"'.format(args.dataSet)
+    command = "dasgoclient --query={0:s}  > nevents.txt".format(query)
+    #out = subprocess.check_output( os.system(command), shell=True)
+    os.system(command)
+    
+
+Nevents=[]
+nev =open('nevents.txt','r').readlines()
+for i, j in enumerate(nev) : Nevents.append(int(j))
+
+    #print 'for ', args.dataSet, 'we have', output
+
+
+
+nevents=sum(Nevents)
+#print '-------------------', Nevents, dataset, nevents
+print nevents
 mjobs = 5
-
+#this will set each job to about 50k on average
+mjobs=int(nevents/50000)
 
 ff = open("../../Files/runjme.sh", "r")
 textf = ff.read()
@@ -80,24 +107,28 @@ for i in range(1, mjobs) :
 
     outFile = '{0:s}_{1:s}_{2:s}of{3:s}.root'.format(str(args.nickName), era, str(i), str(mjobs-1) )
 
-    outLines.append("    config.General.requestName = '{0:s}_part{1:s}' \n".format(args.nickName, str(i)))
+    outLines.append("    config.General.workArea = '../../Crab_projects_{0:s}_newJECs' \n".format(era) )
+    outLines.append("    config.General.requestName = '{0:s}_{1:s}_part{2:s}' \n".format(args.nickName, era, str(i)))
     outLines.append("    config.Data.outLFNDirBase = '/store/group/lpcsusyhiggs/ntuples/nAODv7/JEC_{0:s}' \n".format(era))
     outLines.append("    config.Data.inputDataset = '{0:s}' \n".format(str(args.dataSet)))
-    outLines.append("    config.Data.outputDatasetTag = '{0:s}' \n".format(outFile))
-    outLines.append("    config.Data.outputFiles = '{0:s}_{1:s}.root' \n".format(str(args.nickName), str(i)))
-    outLines.append("    config.Data.scriptExe = '/uscms_data/d3/alkaloge/ntuples/CMSSW_10_6_4/src/test/{0:s}/{1:s}_{2:s}/part_{3:s}.sh' \n".format(args.selection, args.nickName, era, str(i)))
-    outLines.append("    config.Data.maxJobRuntimeMin= 3000 \n") 
+    outLines.append("    config.Data.outputDatasetTag = '{0:s}_{1:s}' \n".format(args.nickName, era))
+    outLines.append("    config.JobType.outputFiles = ['{0:s}'] \n".format(outFile))
+    outLines.append("    config.JobType.scriptExe = '/uscms_data/d3/alkaloge/ntuples/CMSSW_10_6_4/src/newCrab/{0:s}/{1:s}_{2:s}/part_{3:s}.sh' \n".format(args.selection, args.nickName, era, str(i)))
+    #outLines.append("    config.JobType.scriptExe = '/uscms_data/d3/alkaloge/ntuples/CMSSW_10_6_4/src/test/test/{1:s}_{2:s}/part_{3:s}.sh' \n".format(args.selection, args.nickName, era, str(i)))
+    outLines.append("    config.JobType.maxJobRuntimeMin = 3000 \n") 
     outLines.append("    config.Data.splitting= 'FileBased' \n") 
     outLines.append("    config.Data.unitsPerJob= 1 \n") 
-    outLines.append("    config.Data.totalUnits= {0:s} \n".format(str(events)))
+    outLines.append("    config.Data.totalUnits= {0:s} \n".format(str(nevents)))
     outLines.append("    submit(config) \n")
 
     runName = "part_{0:s}.sh".format(str(i))
     runLines=['#!/usr/local/bin/python \n']
     runLines.append(textf)
-    start = i*0.2
-    finish = (i+1) * 0.2
-    if i== mjobs-1 : finish +=0.05
+    start= '%.2f' %(i/mjobs)
+    finish = '%.2f' %((i+1)/mjobs)
+    print start, finish
+    if i== mjobs-1 : finish = 1.05
+
     runLines.append("sed -i "'"s/STARTEVENT/{0:s}/g"'" make_jmev2.py \n" .format(str(start)) )
     runLines.append("sed -i "'"s/FINISHEVENT/{0:s}/g"'" make_jmev2.py \n" .format(str(finish)) )
     runLines.append("python make_jmev2.py True 2016 B \n" .format(str(start)) )
