43c62ea6aba0c482557f2ac3a9659a64b5a82b3e
Alexis <alkaloge@cern.ch>
2020-11-10 05:37:53 -0600

    crab tools

--- README.cmd
+++ README.cmd
@@ -2,7 +2,7 @@ Some CRAB tools to run the JME nAODTools to get systeamatics
 
 python prepapereJobs.py -y year -t tag -s dosystematics -l islocal -t tag -j JobsPerFile
 
-once the .csh is created, you can  execute it and it will submit crab jobs.
+once the .csh is created, you can  execute it and it will submit crab jobs by executing >doSend.sh tag
 
 Edit Files/keep_and_drop.txt to add/remove branches that you don't need
 
